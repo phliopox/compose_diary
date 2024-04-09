1620a18afaf9aa6551e4aa3009c5c3a3452ffeb6
phliopox <91457591+phliopox@users.noreply.github.com>
2024-04-09 17:46:44 +0900

    REFACTOR : Util

--- app/src/main/java/com/example/gradientdiary/presentation/Util.kt
+++ app/src/main/java/com/example/gradientdiary/presentation/Util.kt
@@ -6,7 +6,6 @@ import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.Locale
 
-fun getNow(): LocalDate =  LocalDate.now()
 
 
 fun getDaysInCurrentMonth() : Int{
@@ -19,22 +18,21 @@ fun getDaysInCurrentMonth() : Int{
 }
 
 fun getFirstDayNameOfMonth():String {
-    val today = getNow()
-    val firstDayOfMonth = LocalDate.of(today.year, today.month, 1)
+    val firstDayOfMonth = LocalDate.of(getYear(), getMonth(), 1)
     val dayOfWeek = firstDayOfMonth.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN)
 
     Timber.e("현재 월의 1일은 $dayOfWeek 입니다.")
     return dayOfWeek
 }
 fun getBlankCountOfMonth():Int {
-    val today = getNow()
-    val firstDayOfMonth = LocalDate.of(today.year, today.month, 1)
+    val firstDayOfMonth = LocalDate.of(getYear(), getMonth(), 1)
     val blankCount = firstDayOfMonth.dayOfWeek.value
 
     Timber.e("달력 블럭을 비워야할 blank day는 $blankCount 개 입니다")
     return blankCount
 }
 
+fun getNow(): LocalDate =  LocalDate.now()
 
 fun getMonth(): Int {
     return getNow().monthValue
