04af6a7cce17cb09c2a7c2d9c792d7d7cf279b1d
phliopox <91457591+phliopox@users.noreply.github.com>
2024-04-11 17:43:29 +0900

    REFACTOR : WriteScreen

--- app/src/main/java/com/example/gradientdiary/presentation/ui/WriteScreen.kt
+++ app/src/main/java/com/example/gradientdiary/presentation/ui/WriteScreen.kt
@@ -3,11 +3,15 @@ package com.example.gradientdiary.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,8 +22,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.gradientdiary.R
 import com.example.gradientdiary.presentation.theme.GradientDiaryTheme
 import com.example.gradientdiary.presentation.theme.Paddings
 
@@ -35,36 +41,62 @@ fun WriteScreen() {
         Column(
             modifier = Modifier.padding(15.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             Spacer(modifier = Modifier.size(50.dp))
-            Text("2024년 4월 11일",
-                style = MaterialTheme.typography.titleMedium)
-            Text("제목",
-                style = MaterialTheme.typography.titleMedium)
+            Text(
+                "2024년 4월 11일",
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                "제목",
+                style = MaterialTheme.typography.titleMedium
+            )
         }
-        BasicTextField(
-            value = text ?: "",
-            onValueChange = { newText ->
-                text = newText
-            },
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 10.dp),
-            textStyle = MaterialTheme.typography.labelSmall,
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier
-                        .background(Color.White)
-                        .padding(
-                            horizontal = Paddings.xlarge,
-                            vertical = Paddings.medium
-                        ), // inner padding
-                ) {
-                    innerTextField()
+        Column(
+            Modifier
+                .weight(1f)
+                .padding(top = 10.dp)
+        ) {
+
+
+            BasicTextField(
+                value = text ?: "",
+                onValueChange = { newText ->
+                    text = newText
+                },
+                 modifier = Modifier
+                     .fillMaxSize(),
+                textStyle = MaterialTheme.typography.labelSmall,
+                decorationBox = { innerTextField ->
+                    Box(
+                        modifier = Modifier
+                            .background(Color.White)
+                            .padding(
+                                horizontal = Paddings.xlarge,
+                                vertical = Paddings.medium
+                            ), // inner padding
+                    ) {
+                        innerTextField()
+                    }
                 }
-            }
-        )
+            )
+        }
+        Row(modifier = Modifier.height(30.dp).fillMaxWidth()) {
+            Icon(
+                painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = null
+            )
+            Icon(
+                painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = null
+            )
+            Icon(
+                painterResource(id = R.drawable.baseline_keyboard_arrow_right_24),
+                contentDescription = null
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true)
