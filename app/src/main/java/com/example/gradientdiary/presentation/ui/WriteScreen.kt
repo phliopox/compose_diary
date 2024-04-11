b69d5e7dcd13d48716f9b83a61b525fd2a68a491
phliopox <91457591+phliopox@users.noreply.github.com>
2024-04-11 17:29:35 +0900

    REFACTOR : WriteScreen

--- app/src/main/java/com/example/gradientdiary/presentation/ui/WriteScreen.kt
+++ app/src/main/java/com/example/gradientdiary/presentation/ui/WriteScreen.kt
@@ -3,11 +3,13 @@ package com.example.gradientdiary.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,27 +29,32 @@ fun WriteScreen() {
     var text by remember { mutableStateOf("") }
 
     Column(
-        //modifier = modifier,
-        horizontalAlignment = Alignment.End
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Column(
+            modifier = Modifier.padding(15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Spacer(modifier = Modifier.size(50.dp))
+            Text("2024년 4월 11일",
+                style = MaterialTheme.typography.titleMedium)
+            Text("제목",
+                style = MaterialTheme.typography.titleMedium)
+        }
         BasicTextField(
             value = text ?: "",
             onValueChange = { newText ->
                 text = newText
             },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 10.dp),
             textStyle = MaterialTheme.typography.labelSmall,
             decorationBox = { innerTextField ->
                 Box(
                     modifier = Modifier
-                        .padding(top = Paddings.small)
-                        /*.border(
-                            width = 1.dp,
-                            color = Grey100,
-                            shape = RoundedCornerShape(size = 5.dp)
-                        )*/
-                        .background(Color.White, shape = RoundedCornerShape(5.dp))
+                        .background(Color.White)
                         .padding(
                             horizontal = Paddings.xlarge,
                             vertical = Paddings.medium
@@ -60,7 +67,7 @@ fun WriteScreen() {
     }
 }
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun PreviewWriteScreen() {
     GradientDiaryTheme {
