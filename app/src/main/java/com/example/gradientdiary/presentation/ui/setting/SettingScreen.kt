d4458358f84f93d7392592c39e9619463a8d4882
phliopox <91457591+phliopox@users.noreply.github.com>
2024-07-09 10:57:20 +0900

    REFACTOR: TextStyleSettingScreen

--- app/src/main/java/com/example/gradientdiary/presentation/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/example/gradientdiary/presentation/ui/setting/SettingScreen.kt
@@ -2,29 +2,23 @@ package com.example.gradientdiary.presentation.ui.setting
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImagePainter.State.Empty.painter
 import com.example.gradientdiary.R
+import com.example.gradientdiary.presentation.theme.DefaultText
 import com.example.gradientdiary.presentation.theme.Dimens
 import com.example.gradientdiary.presentation.theme.GradientDiaryTheme
 
@@ -43,7 +37,7 @@ fun SettingScreen(
     handleLanguageSetting: () -> Unit,
     handleReviewIntent : ()->Unit
 ) {
-
+    val textStyle =  MaterialTheme.typography.titleLarge.copy(color = DefaultText)
     val interactionSource = remember { MutableInteractionSource() }
     val iconModifier = Modifier
         .size(40.dp)
@@ -88,9 +82,9 @@ fun SettingScreen(
                     handleTextStyleSetting()
                 },
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            //horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text("글자 설정")
+            Text("글자 설정" , style = textStyle , modifier = Modifier.padding(end = Dimens.dp5))
 
             Icon(
                 modifier = iconModifier,
--- app/src/main/java/com/example/gradientdiary/presentation/ui/setting/TextStyleSettingScreen.kt
+++ app/src/main/java/com/example/gradientdiary/presentation/ui/setting/TextStyleSettingScreen.kt
@@ -1,7 +1,9 @@
 package com.example.gradientdiary.presentation.ui.setting
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,21 +13,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.gradientdiary.R
 import com.example.gradientdiary.data.storage.getFontResource
+import com.example.gradientdiary.presentation.theme.Dimens
+import com.example.gradientdiary.presentation.theme.Grey70
 import com.example.gradientdiary.presentation.viewModel.SettingViewModel
-import timber.log.Timber
 
 val fontNames = listOf(
     "restart",
@@ -36,6 +45,7 @@ val fontNames = listOf(
     "adultkid",
     "kopub_worlddotum"
 )
+
 @Composable
 fun TextStyleSettingScreen(
     settingViewModel: SettingViewModel
@@ -50,52 +60,77 @@ fun TextStyleSettingScreen(
             Column(
                 Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(Dimens.dp20)
+                    .clip(RoundedCornerShape(Dimens.dp8))
+                    .border(Dimens.dp1, Grey70, RoundedCornerShape(Dimens.dp8))
+                    .padding(Dimens.dp30),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "글꼴 미리보기", style = previewTextStyle)
-                Spacer(modifier = Modifier.height(16.dp))
-                /*     Spacer(modifier = Modifier.height(16.dp))
-                     Button(onClick = { settingViewModel.applyFontSelection() }) {
-                         Text(text = "적용")
-                     }
-                     Spacer(modifier = Modifier.height(16.dp))*/
-                Text("오늘도 다양한 기록을 카테고리로 분류해서 보관해요", style = previewTextStyle)
+                Text(
+                    "카테고리로 분류하여 기록할 수 있는 다이어리,\n${stringResource(id = R.string.app_name)} 입니다 !\n글꼴을 선택하여 스타일을 미리 확인해보세요 ^^",
+                    style = previewTextStyle,
+                    fontSize = 20.sp
+                )
             }
         }
-        this@LazyColumn.fontSelectionColumn(selectedFont,onFontSelected = settingViewModel::updateFontSelection)
+        this@LazyColumn.fontSelectionColumn(
+            selectedFont,
+            onFontSelected = settingViewModel::updateFontSelection
+        )
     }
 }
-
 
 
 fun LazyListScope.fontSelectionColumn(selectedFont: String, onFontSelected: (String) -> Unit) {
 
+    item{
+        Spacer(modifier = Modifier.height(Dimens.dp20))
+        Divider(
+            color = Grey70,
+            modifier = Modifier
+                .height(Dimens.dp1)
+                .padding(horizontal = Dimens.dp20)
+                .fillMaxWidth()
+        )
+    }
     items(fontNames) {
         val fontResource = FontFamily(Font(getFontResource(it)))
         val isSelected = it == selectedFont
-
-
-        Row(modifier = Modifier.clickable(
-            MutableInteractionSource(),
-            null
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = Dimens.dp20)
+                .clickable(
+                    MutableInteractionSource(),
+                    null
+                ) {
+                    onFontSelected(it)
+                },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            onFontSelected(it)
-        }) {
-            Text(getFontName(it),fontFamily = fontResource,
-                fontSize = 18.sp)
-            if (isSelected) {
-                Timber.e("icon cleked ")
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_check_24),
-                    contentDescription = "Selected"
-                )
-            }
+            Text(
+                getFontName(it), fontFamily = fontResource,
+                fontSize = 18.sp
+            )
+            RadioButton(
+                selected = isSelected,
+                onClick = { onFontSelected(it) },
+                colors = RadioButtonDefaults.colors(selectedColor = Color.Black)
+            )
         }
+        Divider(
+            color = Grey70,
+            modifier = Modifier
+                .height(1.dp)
+                .padding(horizontal = Dimens.dp20)
+                .fillMaxWidth()
+        )
     }
 }
 
-fun getFontName(str : String) : String{
-    return when(str){
+fun getFontName(str: String): String {
+    return when (str) {
         "restart" -> "나눔 손글씨 다시 시작해체"
         "saeenum" -> "강원교육새움체"
         "ongeul_julison" -> "온글잎 주리 손편지체"
